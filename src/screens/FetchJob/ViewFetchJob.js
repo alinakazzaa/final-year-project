c222486c964fe325c75e76450a3f2b8e8738a41f
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-01-25 21:10:46 +0100

    further project structure

---
+++ src/screens/FetchJob/ViewFetchJob.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { View, Text, YellowBox, StyleSheet } from 'react-native';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
+import { createStackNavigator } from 'react-navigation-stack';
+
+
+class ViewFetchJob extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>View Fetch Job</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        text: {
+            textAlign: 'center',
+            color: 'black'
+        }
+    });
+
+export default ViewFetchJob
