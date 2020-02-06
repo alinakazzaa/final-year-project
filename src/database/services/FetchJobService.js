beb5604316b356251e9487e8eb4aa73e8d01986b
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-06 19:56:01 +0000

    created fetch job service

--- src/database/services/FetchJobService.js
+++ src/database/services/FetchJobService.js
@@ -1,13 +1,14 @@
 import { db } from '../config/db';
 
-export const addFetchJob = (user_id, project_id, fetchJob) => {
+export const addFetchJob = (user_id, project_id, fetch_job) => {
     db.ref(`/Users/${user_id}/Projects/${project_id}/FetchJobs`).push({
-        title: fetchJob.title,
-        date_created: fetchJob.date_created,
-        hashtag: fetchJob.hashtag,
-        location: fetchJob.location,
-        criteria: String(fetchJob.criteria)
+        title: fetch_job.title,
+        date_created: fetch_job.date_created,
+        hashtag: fetch_job.hashtag,
+        location: fetch_job.location,
+        criteria: String(fetch_job.active_criteria)
     });
+
 }
 
 export const removeFetchJob = (user_id, project_id, fetchJob_id) => {
