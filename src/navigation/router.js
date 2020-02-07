cd27de478e3d9d08a576735205ca74d085867f78
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-07 20:27:27 +0000

    configure router with redux state

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import {
-    AppRegistry,
-    Text,
-} from 'react-native';
-
-import { createStackNavigator } from 'react-navigation-stack';
-
-import HomeScreen from '../screens/Home/Home';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { createAppContainer } from 'react-navigation';
-import { BottomNavigator } from './BottomNavigation'
+import * as userActions from '../actions/user';
+import { ApplicationStack } from './ApplicationStack';
+import { BottomNavigator } from './BottomNavigation';
 
-const RouteContainer = createAppContainer(BottomNavigator);
+const LogIn = createAppContainer(ApplicationStack);
+const MainApp = createAppContainer(BottomNavigator);
 
-export default class App extends React.Component {
+class Router extends Component {
     render() {
-        return <RouteContainer />;
+        let { user } = this.props
+        const current = { ...user }
+        console.log(current.details && "current user: " + current.details.username || "no user logged in")
+        return (
+            !current.details ? <LogIn /> : <MainApp />
+        );
     }
-}
+};
+
+const mapStateToProps = state => ({
+    user: state.user
+});
+
+const ActionCreators = Object.assign(
+    {},
+    userActions
+);
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(ActionCreators, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Router)
+
