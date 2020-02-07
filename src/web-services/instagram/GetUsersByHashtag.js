aa41f1f40b87cd10c264c76b63d4487c593f2713
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-07 12:09:08 +0000

    created get influencers by hashtag service

--- src/web-services/instagram/GetUsersByHashtag.js
+++ src/web-services/instagram/GetUsersByHashtag.js
@@ -1,52 +1,63 @@
-import * as React from 'react';
-import { View, Text, YellowBox, StyleSheet } from 'react-native';
-import { addInfluData } from '../../database/services/InfluencerService';
-
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-
 const HASHTAG_OBJECT = "graphql"
 const HASHTAG = "hashtag"
 const MEDIA_COUNT = "count"
 const ALL_POSTS = "edge_hashtag_to_media"
 const TOP_POSTS = "edge_hashtag_to_top_posts"
 const POSTS_NODE = "edges"
+import { db } from '../../database/config/db';
+import { addInfluencerByIdHashtagAll, addInfluencerByIdHashtagTop } from '../../database/services/InfluencerService'
+import { getInfluencerData } from './GetInfluencerData'
 
+export const getInfluencersByHashtag = tag => {
 
-class GetUsersByHashtag extends React.Component {
+    let all = []
+    let top = []
+    let related = []
+    let all_posts = []
+    let top_posts = []
+    let related_tags = []
+    let hashtag = ''
 
-    getUsersByHashtag = tag => {
-        fetch(`https://www.instagram.com/explore/tags/${tag}/?__a=1`, {
-            // try not to overpower the API, fetch precise tags for optimal speed etc. 
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseJson) => {
-                addInfluData(responseJson) //add hashtag data to DB
+    fetch(`https://www.instagram.com/explore/tags/${tag}/?__a=1`, {
+        // try not to overpower the API, fetch precise tags for optimal speed etc. 
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            hashtag = responseJson.graphql.hashtag.name
+            responseJson.graphql.hashtag.edge_hashtag_to_media.edges.forEach(elem => {
+                addInfluencerByIdHashtagAll(elem.node.owner.id, hashtag)
+                getInfluencerData(elem.node.owner.id, hashtag)
             })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-
-    componentDidMount() {
-        this.getUsersByHashtag()
-    }
-
-    render() {
-        return (
-            <View>
-
-            </View>
-        );
-    }
+            responseJson.graphql.hashtag.edge_hashtag_to_top_posts.edges.forEach(elem => {
+                addInfluencerByIdHashtagTop(elem.node.owner.id, hashtag)
+                getInfluencerData(elem.node.owner.id, hashtag)
+            })
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
-const styles = StyleSheet.create(
-    {}
-);
+// componentDidMount() {
+//     this.getUsersByHashtag()
+// }
 
-export default GetUsersByHashtag
+// render() {
+//     return (
+//         <View>
+
+//         </View>
+//     );
+// }
+// }
+
+// const styles = StyleSheet.create(
+//     {}
+// );
+
+// export default GetUsersByHashtag
