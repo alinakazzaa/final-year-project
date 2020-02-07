ea56ff28ca1c7b25478bfea321d02ad208ffee2b
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-07 20:20:35 +0000

    added redux state to index.js

--- index.js
+++ index.js
@@ -2,9 +2,41 @@
  * @format
  */
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
 import Router from './src/navigation/router';
+import configureStore from './src/store/configureStore';
 
-AppRegistry.registerComponent(appName, () => Router);
+const store = configureStore()
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <Router />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => RNRedux);
+// AppRegistry.registerComponent(appName, () => Router);
+
+// AppRegistry.registerComponent(appName, () => App);
+
+// import { AppRegistry } from 'react-native';
+// import React from 'react';
+// import App from './App';
+// import { name as appName } from './app.json';
+// import { Provider } from 'react-redux';
+
+// import configureStore from './src/store/configureStore';
+
+// const store = configureStore()
+
+// const RNRedux = () => (
+//     <Provider store={store}>
+//         <App />
+//     </Provider>
+// )
+
+// AppRegistry.registerComponent(appName, () => RNRedux);
