c755384528f4df3fafc4a01a3edfd0298acbd667
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-08 22:02:45 +0000

    removed project reducer stuff

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,34 +1,21 @@
-import { USER_LOGIN, USER_LOGOUT, SET_USER_PROJECTS } from '../constants';
+import { USER_LOGIN, USER_LOGOUT, SET_USER_PROJECTS, SET_CURRENT_PROJECT } from '../constants';
 
 const initialState = {
     user: {
-        details: {},
-        projects: {},
-        fetch_jobs: {},
-        collaborations: {},
     }
 };
+
 const userReducer = (state = initialState, action) => {
-    let updated_user = { ...state.user }
-    let updated_state = { ...state }
     switch (action.type) {
         case USER_LOGIN:
-            const details = { ...action.payload.details }
-            updated_user.details = details
-            updated_state.user = updated_user
+            // let user = { ...state.user }
+            // user.details = { ...action.payload }
             return {
-                ...updated_state
+                ...action.payload
             };
         case USER_LOGOUT:
             return {
                 ...initialState
-            };
-        case SET_USER_PROJECTS:
-            const projects = [...action.payload]
-            updated_user.projects = projects
-            updated_state.user = updated_user
-            return {
-                ...updated_state
             };
         default:
             return state;
