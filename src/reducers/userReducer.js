84dbe81701857f8b0103842bb844b4cc565886bb
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-08 17:51:06 +0000

    (with error) but getting user projs into state

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,19 +1,34 @@
-import { USER_LOGIN, USER_LOGOUT } from '../constants';
+import { USER_LOGIN, USER_LOGOUT, SET_USER_PROJECTS } from '../constants';
 
 const initialState = {
-    user: {}
+    user: {
+        details: {},
+        projects: {},
+        fetch_jobs: {},
+        collaborations: {},
+    }
 };
 const userReducer = (state = initialState, action) => {
-
+    let newState = { ...state }
+    let newUser = { ...state.user }
     switch (action.type) {
         case USER_LOGIN:
+            const user = { ...action.payload }
+            newUser.details = user.details
             return {
-                ...state,
-                ...action.payload
+                ...newUser
             };
         case USER_LOGOUT:
             return {
-                ...action.payload
+                ...initialState
+            };
+        case SET_USER_PROJECTS:
+            const projects = [...action.payload]
+            // let newProjects = newState.projects
+            newState.projects = projects
+            // newProjects = projects
+            return {
+                ...newState
             };
         default:
             return state;
