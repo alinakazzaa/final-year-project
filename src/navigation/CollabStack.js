63e454dfda94d10217234f77ec44801f878e21e6
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-08 17:50:19 +0000

    created collabs stack

---
+++ src/navigation/CollabStack.js
@@ -0,0 +1,25 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import AllCollabs from '../screens/Collab/AllCollabs';
+import AddCollab from '../screens/Collab/AddCollab';
+import EditCollab from '../screens/Collab/EditCollab';
+import ViewCollab from '../screens/Collab/ViewCollab';
+import ViewFetchJob from '../screens/FetchJob/ViewFetchJob';
+import AllFetchJobs from '../screens/FetchJob/AllFetchJobs';
+import AddFetchJob from '../screens/FetchJob/AddFetchJob';
+import AllInfluencers from '../screens/Influencer/AllInfluencers';
+import ViewInfluencer from '../screens/Influencer/ViewInfluencer';
+
+export const CollabStack = createStackNavigator({
+    AllFetchJobs: { screen: AllFetchJobs },
+    ViewFetchJob: { screen: ViewFetchJob },
+    AddFetchJob: { screen: AddFetchJob },
+    AllInfluencers: { screen: AllInfluencers },
+    ViewInfluencer: { screen: ViewInfluencer },
+    AllCollabs: { screen: AllCollabs },
+    ViewCollab: { screen: ViewCollab },
+    AddCollab: { screen: AddCollab },
+    EditCollab: { screen: EditCollab },
+},
+    {
+        initialRouteName: 'AllCollabs'
+    });
