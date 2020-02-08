3ebbfbebcadbc484369c6d7065bcdd9025bcab82
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-08 17:51:50 +0000

    get used projects in all projects screen

--- src/screens/Project/AllProjects.js
+++ src/screens/Project/AllProjects.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, YellowBox, StyleSheet, TouchableOpacity, Keyboard } from 'react-native';
+import { View, Text, YellowBox, StyleSheet, TouchableOpacity, Keyboard, ActivityIndicator } from 'react-native';
 import { db } from '../../database/config/db';
 import { AppHeader } from '../../layouts/Header';
 import { IconButton } from '../../components/buttons/IconButton';
@@ -8,14 +8,18 @@ import { TabView, SceneMap } from 'react-native-tab-view';
 import { ProjectList } from '../../components/list/ProjectList';
 import { Input } from 'react-native-elements';
 import { removeProject } from '../../database/services/ProjectService'
+import * as userActions from '../../actions/user';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { DB_USER_REF, DB_PROJECTS_REF, DB_FETCH_JOBS_REF, DB_INFLUENCERS_REF } from '../../constants/index'
+import { updateUser } from '../../database/services/UserService';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-
-let usersRef = db.ref('/Users');
 
 class AllProjects extends React.Component {
 
     state = {
+        isLoading: false,
         projects: [],
         index: 0,
         routes: [
@@ -25,22 +29,33 @@ class AllProjects extends React.Component {
     }
 
     componentDidMount() {
-        usersRef.on('value', (snapshot) => {
-            snapshot.forEach(childSnapshot => {
-                if (childSnapshot.val().username == 'alinakazzaa') {
-                    let projectsRef = usersRef.child(`${childSnapshot.key}/Projects`)
-                    projectsRef.on('value', (snapshot) => {
-                        let data = snapshot.val();
-                        let projects = Object.values(data);
-                        this.setState({ projects });
-                    });
+        this.setState({ isLoading: true })
+        let { actions, user } = this.props;
+        console.log(user)
+        let projects = []
+        let projects_ref
+        DB_USER_REF.on('value', u_snap => {
+            u_snap.forEach(item => {
+                if (item.key == user.details.id) {
+                    projects_ref = `Users/${item.key}/Projects`
+                    db.ref(projects_ref).on('value', p_snap => {
+                        p_snap.forEach(proj => {
+                            let data = proj.val()
+                            projects = Object.values(data)
+                        })
+                    })
                 }
+
             })
-        });
+        })
+        actions.setUserProjects(projects)
+        this.setState({ isLoading: false })
+
     }
 
     addProject = () => {
-        this.props.navigation.navigate('AddProject')
+        let { user } = this.props;
+        this.props.navigation.navigate('AddProject', { user })
     }
 
     goToProject = proj => {
@@ -66,6 +81,12 @@ class AllProjects extends React.Component {
     }
 
     render() {
+        // const { user, projects } = this.props;
+        // let { actions, user, projects, state } = this.props;
+        // let s = { ...state }
+        // console.log(user)
+        // console.log(user)
+        // console.log(projects)
         return (
             // <View style={styles.container}>
             //     <Text style={styles.text}>All Projects</Text>
@@ -76,34 +97,51 @@ class AllProjects extends React.Component {
             //         <Text>View Project</Text>
             //     </TouchableOpacity>
             // </View>
-            <View style={styles.main}>
-                <AppHeader
-                    left={
-                        <IconButton color="#5d4d50'"
-                            type='font-awesome'
-                            name='angle-left'
-                            size={40}
-                            onPress={() => this.props.navigation.goBack()}
-                        />}
-                    center={<Input
-                        placeholder="Search projects"
-                        placeholderTextColor="#b3b3cc"
-                        inputContainerStyle={styles.inputStyle}
-                        inputStyle={styles.txtStyle}
-                        onChangeText={(text) => this.searchProject(text)}
-                    />}
-                    right={<View style={styles.cancelBtn}><TextButton title="Cancel" onPress={Keyboard.dismiss} /></View>}
-                />
-                <TabView
-                    navigationState={this.state}
-                    renderScene={SceneMap({
-                        active: () => <ProjectList addProject={this.addProject} goToProject={this.goToProject} deleteProject={this.deleteProject} active projects={this.state.projects} />,
-                        archived: () => <ProjectList deleteProject={this.deleteProject} goToProject={this.goToProject} active={false} projects={this.state.projects} />,
-                    })}
-                    onIndexChange={index => this.setState({ index })}
-                    initialLayout={{ width: 250, height: 250 }}
-                />
 
+            <View style={styles.main} >
+                {
+                    this.state.isLoading ? <View style={styles.loading}>
+                        <ActivityIndicator size="large" color="#5d4d50" />
+                        <Text style={styles.loadingTxt}>Wait, getting influencers for you</Text>
+                    </View> : <View>
+                            <AppHeader
+                                left={
+                                    <IconButton color="#5d4d50'"
+                                        type='font-awesome'
+                                        name='angle-left'
+                                        size={40}
+                                        onPress={() => this.props.navigation.goBack()}
+                                    />}
+                                center={<Input
+                                    placeholder="Search projects"
+                                    placeholderTextColor="#b3b3cc"
+                                    inputContainerStyle={styles.inputStyle}
+                                    inputStyle={styles.txtStyle}
+                                    onChangeText={(text) => this.searchProject(text)}
+                                />}
+                                right={<View style={styles.cancelBtn}><TextButton title="Cancel" onPress={Keyboard.dismiss} /></View>}
+                            />
+                            <TabView
+                                navigationState={this.state}
+                                renderScene={SceneMap({
+                                    active: () => <ProjectList goToProject={this.goToProject} deleteProject={this.deleteProject} active projects={this.state.projects} />
+                                    ,
+                                    archived: () => <ProjectList deleteProject={this.deleteProject} goToProject={this.goToProject} active={false} projects={this.state.projects} />,
+                                })}
+                                onIndexChange={index => this.setState({ index })}
+                                initialLayout={{ width: 250, height: 250 }}
+                            />
+
+                            <IconButton
+                                color="#493649"
+                                name="plus"
+                                type='font-awesome'
+                                size={40}
+                                onPress={this.addProject}
+                                style={styles.addIcon}
+                            />
+                        </View>
+                }
             </View>
         );
     }
@@ -117,7 +155,12 @@ const styles = StyleSheet.create(
             display: 'flex',
             flex: 4,
             flexDirection: 'column',
-
+        },
+        loading: {
+            display: 'flex',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
         },
         txtStyle: {
             fontFamily: 'Arial',
@@ -138,6 +181,28 @@ const styles = StyleSheet.create(
         scene: {
             flex: 1,
         },
+        loadingTxt: {
+            fontFamily: 'Arial',
+            fontSize: 15,
+            color: '#5d4d50',
+            padding: '4%'
+        },
+        addIcon: {
+            alignSelf: 'center',
+            backgroundColor: 'transparent'
+        },
     });
 
-export default AllProjects
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+const ActionCreators = Object.assign(
+    {},
+    userActions
+);
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(ActionCreators, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProjects)
