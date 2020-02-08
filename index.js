5a43281d7e6a91f8370ffe1adcb2d2520638276b
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-08 00:44:32 +0000

    removed commented out code

--- index.js
+++ index.js
@@ -19,24 +19,3 @@ const RNRedux = () => (
 )
 
 AppRegistry.registerComponent(appName, () => RNRedux);
-// AppRegistry.registerComponent(appName, () => Router);
-
-// AppRegistry.registerComponent(appName, () => App);
-
-// import { AppRegistry } from 'react-native';
-// import React from 'react';
-// import App from './App';
-// import { name as appName } from './app.json';
-// import { Provider } from 'react-redux';
-
-// import configureStore from './src/store/configureStore';
-
-// const store = configureStore()
-
-// const RNRedux = () => (
-//     <Provider store={store}>
-//         <App />
-//     </Provider>
-// )
-
-// AppRegistry.registerComponent(appName, () => RNRedux);
