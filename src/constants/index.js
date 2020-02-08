8fa9471a194d167a398fd2dd83e8e427dc752067
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-08 17:49:07 +0000

    changed name of const

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,6 +6,7 @@ export const USER_LOGIN = 'USER_LOGIN'
 export const USER_LOGOUT = 'USER_LOGOUT'
 export const SET_USER_PROJECTS = 'SET_USER_PROJECTS'
 
+
 export const DB_USER_REF = db.ref('Users/');
 export const DB_PROJECTS_REF = 'Projects'
 export const DB_FETCH_JOBS_REF = 'FetchJobs'
