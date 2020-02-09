6b3540c5a42681b522eec0d170c3065497afa77a
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-09 00:05:13 +0000

    set current fetch job

--- src/actions/fetchJob.js
+++ src/actions/fetchJob.js
@@ -1,9 +1,16 @@
-import { SET_FETCH_JOBS } from '../constants';
+import { SET_FETCH_JOBS, SET_CURRENT_FETCH_JOB } from '../constants';
 
 export const setFetchJobs = fetch_jobs => {
     return {
         type: SET_FETCH_JOBS,
         payload: fetch_jobs
+    }
+}
+
+export const setCurrentFetchJob = fetch_job => {
+    return {
+        type: SET_CURRENT_FETCH_JOB,
+        payload: fetch_job
     }
 }
 
