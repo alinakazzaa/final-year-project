32284abac1acb0705fa1a59d47b959e0fb3c7682
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-10 16:53:33 +0000

    removed unused functions & changed addInfluencer

--- src/database/services/InfluencerService.js
+++ src/database/services/InfluencerService.js
@@ -1,31 +1,9 @@
 import { db } from '../config/db';
 
-export const addInfluencerByIdHashtagAll = (id, hashtag) => {
-    db.ref(`/Influencers/allposts/hashtags/${hashtag}`).child(id).set({
-        username: '',
-        profileURL: '',
-        followers: 0,
-        media_count: 0
+export const addInfluencer = (influencer, hashtag) => {
+    // console.log(influencer, hashtag)
+    db.ref(`/Influencers/hashtags/${hashtag}`).child(influencer.id).set({
+        ...influencer
     })
 }
 
-export const addInfluencerByIdHashtagTop = (id, hashtag) => {
-    db.ref(`/Influencers/topposts/hashtags/${hashtag}`).child(id).set({
-        username: '',
-        profileURL: '',
-        followers: 0,
-        media_count: 0
-    })
-}
-
-// export const updateProject = (user_id, project_id, project) => {
-//     db.ref(`/Users/${user_id}/Projects/${project_id}`).update({
-//         title: project.title,
-//         description: project.description,
-//         active: project.active
-//     });
-// }
-
-// export const removeProject = (user_id, project_id) => {
-//     db.ref(`/Users/${user_id}/Projects`).child(project_id).remove()
-// }
