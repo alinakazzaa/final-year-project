0bf5be11bd9a23db153229e1460295537c73b742
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-11 13:29:50 +0000

    removed console stmnt

--- src/database/services/InfluencerService.js
+++ src/database/services/InfluencerService.js
@@ -2,7 +2,6 @@ import { db } from '../config/db';
 import { DATE_TODAY } from '../../constants/TodayDate'
 
 export const addInfluencer = (influencer, hashtag) => {
-    // console.log(influencer, hashtag)
     db.ref(`/Influencers/hashtags/${hashtag}`).child(influencer.id).set({
         ...influencer, date_added: DATE_TODAY
     })
