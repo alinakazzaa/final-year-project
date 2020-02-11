b34d147390c9b6f40337d4e97fb2907dad939c91
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-11 13:29:40 +0000

    add influencer service

--- src/database/services/InfluencerService.js
+++ src/database/services/InfluencerService.js
@@ -1,9 +1,10 @@
 import { db } from '../config/db';
+import { DATE_TODAY } from '../../constants/TodayDate'
 
 export const addInfluencer = (influencer, hashtag) => {
     // console.log(influencer, hashtag)
     db.ref(`/Influencers/hashtags/${hashtag}`).child(influencer.id).set({
-        ...influencer
+        ...influencer, date_added: DATE_TODAY
     })
 }
 
