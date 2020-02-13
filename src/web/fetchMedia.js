663ea070ce93dded49fd7feb3f0a516951f3406d
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-13 17:22:09 +0000

    created fetch media function component

---
+++ src/web/fetchMedia.js
@@ -0,0 +1,23 @@
+import { getInitialCursorPending, getInitialCursorSuccess, getInitialCursorError, getCurrentPage } from '../actions/instagram';
+import { INSTAGRAM_GET_MEDIA_BY_HASHTAG } from '../constants/endpoints';
+
+const fetchMedia = hashtag => {
+    return dispatch => {
+        dispatch(getInitialCursorPending());
+        fetch(INSTAGRAM_GET_MEDIA_BY_HASHTAG(hashtag))
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    throw (res.error);
+                }
+                // dispatch(getInitialCursorSuccess(res));
+                dispatch(getCurrentPage(res));
+                return res;
+            })
+            .catch(error => {
+                dispatch(getInitialCursorError(error));
+            })
+    }
+}
+
+export default fetchMedia;
