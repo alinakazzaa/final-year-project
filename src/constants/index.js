05c2589417a2aecc637df6780c34d94d119bf200
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-13 17:21:32 +0000

    added DB constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,8 +16,31 @@ export const GET_ALL_FETCH_JOBS = 'GET_ALL_FETCH_JOBS'
 
 export const DB_USER_REF = db.ref('Users/');
 export const DB_USER_PROJECTS_REF = user_id => db.ref(`Users/${user_id}/Projects`);
-export const DB_PROJECT_FETCH_JOBS_REF = (user_id, project_id) => db.ref(`Users/${user_id}/Projects/${project_id}/FetchJobs`);
-export const DB_PROJECTS_REF = 'Projects'
-export const DB_FETCH_JOBS_REF = 'FetchJobs'
-export const DB_INFLUENCERS_REF = 'Influencers'
+export const DB_PROJECT_FETCH_JOBS_REF = (user_id, project_id) => db.ref(`Users//${user_id}/Projects/${project_id}/FetchJobs`);
+
+export const DB_FETCH_JOBS_REF = 'FETCHJOBS'
+export const DB_INFLUENCERS_REF = 'INFLUENCERS'
+
+export const GET_MEDIA_BY_HASHTAG_SUCCESS = 'GET_MEDIA_BY_HASHTAG_SUCCESS'
+export const GET_MEDIA_BY_HASHTAG_ERROR = 'GET_MEDIA_BY_HASHTAG_ERROR'
+export const GET_MEDIA_BY_HASHTAG_PENDING = 'GET_MEDIA_BY_HASHTAG_PENDING'
+export const GET_NEXT_PAGE_SUCCESS = 'GET_NEXT_PAGE_SUCCESS'
+export const GET_NEXT_PAGE_ERROR = 'GET_NEXT_PAGE_ERROR'
+export const GET_NEXT_PAGE_PENDING = 'GET_NEXT_PAGE_PENDING'
+export const GET_USER_BY_ID_SUCCESS = 'GET_USER_BY_ID_SUCCESS'
+export const GET_USER_BY_ID_ERROR = 'GET_USER_BY_ID_ERROR'
+export const GET_USER_BY_ID_PENDING = 'GET_USER_BY_ID_PENDING'
+export const GET_USER_BY_USERNAME_SUCCESS = 'GET_USER_BY_USERNAME_SUCCESS'
+export const GET_USER_BY_USERNAME_ERROR = 'GET_USER_BY_USERNAME_ERROR'
+export const GET_USER_BY_USERNAME_PENDING = 'GET_USER_BY_USERNAME_PENDING'
+
+
+export const GET_MEDIA_IDS = 'GET_MEDIA_IDS'
+
+export const GET_CURRENT_PAGE = 'GET_NEXT_PAGE'
+export const GET_NEXT_PAGE = 'GET_NEXT_PAGE'
+export const GET_USER_BY_ID = 'GET_USER_BY_ID'
+
+
+
 
