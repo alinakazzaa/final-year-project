cc815d745a5296ae40d4ea735fe0d3695b62245c
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-13 17:22:34 +0000

    added instagram reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,20 +1,25 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import userReducer from '../reducers/userReducer';
 import projectReducer from '../reducers/projectReducer';
 import fetchJobReducer from '../reducers/fetchJobReducer';
 import influencerReducer from '../reducers/influencerReducer';
+import instagramReducer from '../reducers/instagramReducer';
+
+const middlewares = [thunk];
 
 const rootReducer = combineReducers(
     {
         user: userReducer,
         project: projectReducer,
         fetch_job: fetchJobReducer,
-        influencer: influencerReducer
+        influencer: influencerReducer,
+        insta_fetch: instagramReducer
     },
 );
 
 const configureStore = () => {
-    return createStore(rootReducer);
+    return createStore(rootReducer, applyMiddleware(...middlewares));
 }
 
 export default configureStore;
