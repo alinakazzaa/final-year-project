95092d2abd2f793533f882632d4b489d62102bba
Alina Kazakova <alina@Alinas-MacBook-Air.local>
2020-02-17 00:19:53 +0000

    changed actions import

--- src/screens/Auth/Registration.js
+++ src/screens/Auth/Registration.js
@@ -2,10 +2,9 @@ import * as React from 'react';
 import { View, Text, YellowBox, StyleSheet, TextInput, Button } from 'react-native';
 import RegistrationForm from '../../components/forms/RegistrationForm';
 import { DB_USER_REF } from '../../constants/index'
-import { addUser, getUserByUsername } from '../../actions/user'
+import { addUser, setLoggedInUserSuccess } from '../../actions/user'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as userActions from '../../actions/user';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
@@ -16,14 +15,14 @@ class RegistrationScreen extends React.Component {
     }
 
     logIn = user => {
-        let { actions } = this.props;
+        const { setLoggedInUserSuccess } = this.props;
         DB_USER_REF.on('value', u_snap => {
             u_snap.forEach(item => {
                 let details = { ...item.val().details }
                 if (details.username == user.username) {
                     let key = item.key
                     let user = { details, id: key }
-                    actions.setLoggedInUser(user)
+                    setLoggedInUserSuccess(user)
                 }
             })
         })
@@ -51,12 +50,8 @@ const mapStateToProps = state => ({
     user: state.user,
 });
 
-const ActionCreators = Object.assign(
-    {},
-    userActions,
-);
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(ActionCreators, dispatch),
-});
+const mapDispatchToProps = dispatch => bindActionCreators({
+    setLoggedInUserSuccess: setLoggedInUserSuccess
+}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(RegistrationScreen)
