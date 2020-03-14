f6cee68e7092ca0e378efcac53ddc2e51cca94e3
Alina Kazakova <alinakazakova@live.ie>
2020-03-14 17:12:26 +0000

    base styling (theme)

---
+++ src/styles/base.js
@@ -0,0 +1,89 @@
+import { Dimensions } from 'react-native'
+
+export const dimensions = {
+    fullHeight: Dimensions.get('window').height,
+    fullWidth: Dimensions.get('window').width
+}
+
+export const colors = {
+    PRIMARY: 'rgb(26, 69, 244)',
+    SECONDARY: 'rgb(223, 32, 220)',
+    TERTIARY: 'rgb(83, 75, 98)',
+    BLACK: 'rgb(27, 23, 37)',
+    WHITE: 'rgb(253, 255, 250)',
+    BORDER: 'rgb(228, 226, 233)'
+}
+
+export const container = {
+    backgroundColor: colors.WHITE,
+    // padding: dimensions.fullHeight * 0.05
+}
+
+export const spacing = {
+    SMALL: 5,
+    MEDIUM: 10,
+    LARGE: 20,
+}
+
+export const width = {
+    SMALL: 50,
+    MEDIUM: 100,
+    LARGE: 200,
+}
+
+export const height = {
+    SMALL: 30,
+    MEDIUM: 50,
+    LARGE: 80,
+}
+
+export const fonts = {
+    SMALL: 14,
+    MEDIUM: 18,
+    LARGE: 20,
+    PRIMARY: 'Arial',
+    WEIGHT_LIGHT: '200',
+    WEIGHT_MEDIUM: '600',
+    WEIGHT_HEAVY: '800'
+}
+
+// components
+
+export const input = {
+    color: colors.BLACK,
+    fontSize: fonts.MEDIUM,
+    fontWeight: fonts.WEIGHT_LEIGHT,
+    height: height.MEDIUM,
+    width: width.LARGE,
+    padding: spacing.MEDIUM,
+    paddingLeft: spacing.LARGE,
+    paddingRight: spacing.LARGE,
+    borderRadius: 25,
+    borderColor: colors.BLACK,
+    borderWidth: 1,
+    marginBottom: spacing.SMALL,
+}
+
+export const inputLabel = {
+    color: colors.BLACK,
+    margin: spacing.SMALL,
+    marginBottom: spacing.MEDIUM,
+    fontSize: fonts.SMALL,
+    textTransform: 'uppercase',
+    fontWeight: fonts.WEIGHT_LEIGHT
+}
+
+export const button = {
+    backgroundColor: 'transparent',
+    padding: spacing.MEDIUM,
+    width: width.LARGE,
+    borderRadius: 25,
+}
+
+export const buttonText = {
+    textAlign: 'center',
+    color: colors.WHITE,
+    fontSize: fonts.LARGE,
+    textTransform: 'uppercase',
+    fontWeight: fonts.WEIGHT_MEDIUM
+}
