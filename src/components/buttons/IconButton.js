47fa27f3b314e28c009e5701fb68da354b7aac33
Alina Kazakova <alinakazakova@live.ie>
2020-03-15 02:28:37 +0000

    removed unused props

--- src/components/buttons/IconButton.js
+++ src/components/buttons/IconButton.js
@@ -4,13 +4,11 @@ import { Icon } from 'react-native-elements';
 import { colors } from '../../styles/base';
 
 
-export const IconButton = ({ type, name, color, size, onPress, raised, style, reverse }) => {
+export const IconButton = ({ type, name, color, size, onPress, style }) => {
     return <Icon
-        raised={raised}
         name={name}
         size={size}
         color={color}
-        reverse={reverse}
         type={type}
         onPress={onPress}
         iconStyle={style}
@@ -22,9 +20,7 @@ IconButton.propTypes = {
     color: PropTypes.string,
     size: PropTypes.number,
     onPress: PropTypes.func,
-    raised: PropTypes.bool,
     style: PropTypes.object,
-    reverse: PropTypes.bool
 }
 
 IconButton.defaultProps = {
@@ -32,8 +28,6 @@ IconButton.defaultProps = {
     color: colors.TERTIARY,
     size: 50,
     onPress: null,
-    raised: false,
     style: null,
-    reverse: false
 }
 
