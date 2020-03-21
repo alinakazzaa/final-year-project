b79eb7e84b0b286af38d87ca9fe91b3b1c99ce13
Alina Kazakova <alinakazakova@live.ie>
2020-03-21 18:06:51 +0000

    back button created

---
+++ src/components/buttons/BackButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { Icon } from 'react-native-elements';
+import { colors } from '../../styles/base';
+
+
+export const BackButton = ({ onPress }) => {
+    return <Icon
+        name='arrow-left'
+        size={35}
+        color={colors.WHITE}
+        type='material-community'
+        onPress={onPress}
+        underlayColor='transparent' />
+}
+
+BackButton.propTypes = {
+    onPress: PropTypes.func,
+    style: PropTypes.object,
+}
+
+BackButton.defaultProps = {
+    onPress: null,
+}
