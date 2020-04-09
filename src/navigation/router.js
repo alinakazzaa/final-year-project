13c23a21e445191a174bf733535cd790469b98df
Alina Kazakova <alinakazakova@live.ie>
2020-04-09 21:38:37 +0100

    router redux state updated

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -1,38 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { createAppContainer } from 'react-navigation';
-import * as userActions from '../actions/user';
-import { ApplicationStack } from './ApplicationStack';
-import { BottomNavigator } from './BottomNavigation';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createAppContainer } from 'react-navigation'
+import { ApplicationStack } from './ApplicationStack'
+import { BottomNavigator } from './BottomNavigation'
 
-const LogIn = createAppContainer(ApplicationStack);
-const MainApp = createAppContainer(BottomNavigator);
+const LogIn = createAppContainer(ApplicationStack)
+const MainApp = createAppContainer(BottomNavigator)
 
 class Router extends Component {
 
     render() {
-        let { user, error, pending } = this.props
+        let { user } = this.props
 
         return (
-            user.username == null ? <LogIn /> : <MainApp />
-        );
+            user.current_user.username == null ? <LogIn /> : <MainApp />
+        )
     }
-};
+}
 
 const mapStateToProps = state => ({
-    state: state,
-    user: state.user.current_user,
-    error: state.user.error,
-    pending: state.user.pending
-});
+    user: state.user
+})
 
-const ActionCreators = Object.assign(
-    {},
-    userActions
-);
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(ActionCreators, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Router)
+export default connect(mapStateToProps)(Router)
