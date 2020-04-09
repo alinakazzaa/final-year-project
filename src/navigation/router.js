c65acdfe7310847b33a060dfb8feea5fe17e305a
Alina Kazakova <alinakazakova@live.ie>
2020-04-09 23:22:38 +0100

    added warning remove code to whole app

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux'
 import { createAppContainer } from 'react-navigation'
 import { ApplicationStack } from './ApplicationStack'
 import { BottomNavigator } from './BottomNavigation'
+import { YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
 
 const LogIn = createAppContainer(ApplicationStack)
 const MainApp = createAppContainer(BottomNavigator)
