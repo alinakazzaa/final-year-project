08baff760d7fff6cd39e8e3b465f5e45278b20b2
Alina Kazakova <alinakazakova@live.ie>
2020-04-13 19:50:18 +0100

    changed to function component

--- src/components/tabview/TabView.js
+++ src/components/tabview/TabView.js
@@ -3,25 +3,20 @@ import PropTypes from 'prop-types'
 import { View, Text, TouchableOpacity } from 'react-native';
 import { twoTabs, threeTabs } from './styles/tabview'
 
-export default class TabView extends React.Component {
+export const TabView = ({ three, titles, index, onPress, color, size }) => {
 
-    render() {
-        const { three, titles, index, onPress, color, size } = this.props
-        let style = twoTabs
+    let style = twoTabs
 
-        if (three) {
-            style = threeTabs
-        }
-
-        return (
-            <View style={style(color, size).container}>
-                <TouchableOpacity onPress={() => onPress(0)} style={index == 0 ? style(color, size).selectedTabItem : style(color, size).tabItem}><Text style={index == 0 ? style(color, size).selectedTab : style(color, size).tab}>{titles[0]}</Text></TouchableOpacity>
-                <TouchableOpacity onPress={() => onPress(1)} style={index == 1 ? style(color, size).selectedTabItem : style(color, size).tabItem}><Text style={index == 1 ? style(color, size).selectedTab : style(color, size).tab}>{titles[1]}</Text></TouchableOpacity>
-                {three && <TouchableOpacity onPress={() => onPress(2)} style={index == 2 ? style(color, size).selectedTabItem : style(color, size).tabItem}><Text style={index == 2 ? style(color, size).selectedTab : style(color, size).tab}>{titles[2]}</Text></TouchableOpacity>}
-
-            </View>
-        )
+    if (three) {
+        style = threeTabs
     }
+
+    return <View style={style(color, size).container}>
+        <TouchableOpacity onPress={() => onPress(0)} style={index == 0 ? style(color, size).selectedTabItem : style(color, size).tabItem}><Text style={index == 0 ? style(color, size).selectedTab : style(color, size).tab}>{titles[0]}</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => onPress(1)} style={index == 1 ? style(color, size).selectedTabItem : style(color, size).tabItem}><Text style={index == 1 ? style(color, size).selectedTab : style(color, size).tab}>{titles[1]}</Text></TouchableOpacity>
+        {three && <TouchableOpacity onPress={() => onPress(2)} style={index == 2 ? style(color, size).selectedTabItem : style(color, size).tabItem}><Text style={index == 2 ? style(color, size).selectedTab : style(color, size).tab}>{titles[2]}</Text></TouchableOpacity>}
+
+    </View>
 }
 
 TabView.propTypes = {
