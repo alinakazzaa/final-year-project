7c04e0b89414191053cbdc222995ef08ec9068ad
Alina Kazakova <alinakazakova@live.ie>
2020-04-13 16:48:27 +0100

    fixed log in actions and reeux store

--- src/screens/Auth/LogIn.js
+++ src/screens/Auth/LogIn.js
@@ -2,11 +2,10 @@ import * as React from 'react';
 import { View } from 'react-native';
 import LogInForm from '../../components/forms/LogInForm';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { setLoggedInUserError, setLoggedInUserSuccess, setUsersPending, getAllUsers } from '../../actions/user';
+import { setCurrentUserPending, setCurrentUserError, setCurrentUserSuccess, setUsersPending, getAllUsers } from '../../actions/user';
 import { auth } from './styles/auth.styles'
 import { Gradient } from '../../styles/Gradient';
-import { MSG_INCORRECT_PASSWORD, MSG_NO_USER } from '../../constants/response/messages';
+import { MSG_INCORRECT_PASSWORD, MSG_NO_USER, MSG_EMPTY_FIELDS } from '../../constants/response/messages';
 import { AppLogo } from '../../components/logo/AppLogo'
 
 class LogInScreen extends React.Component {
@@ -21,29 +20,26 @@ class LogInScreen extends React.Component {
         getAllUsers()
     }
 
-    componentDidUpdate(prev) {
-        // if (prev.users != users) {
-        //     let user_obj = users.find(u => u.username == "A") || {}
-        //     setLoggedInUserSuccess(user_obj)
-        // }
-    }
-
     goToRegister = () => {
         this.props.navigation.navigate("Registration")
     }
 
-    logIn = login_user => {
-        const { setLoggedInUserSuccess, setLoggedInUserError, user } = this.props;
-        let user_obj
+    logIn = loginUser => {
+        const { setCurrentUserPending, setCurrentUserSuccess, setCurrentUserError, user } = this.props
+        setCurrentUserPending()
 
-        if (login_user.username == null || login_user.username == '') {
-            setLoggedInUserError(MSG_NO_USER)
+        if (loginUser.username == null || loginUser.password == null) {
+            setCurrentUserError(MSG_EMPTY_FIELDS)
         } else {
-            user_obj = user.all_users.find(u => u.username == login_user.username) || {}
-            if (login_user.password == null || login_user.password == '' || login_user.password != user_obj.password) {
-                setLoggedInUserError(MSG_INCORRECT_PASSWORD)
+            let found_user = user.all_users.find(u => u.username == loginUser.username)
+
+            if (found_user) {
+                if (loginUser.password == found_user.password)
+                    setCurrentUserSuccess(found_user)
+                else
+                    setCurrentUserError(MSG_INCORRECT_PASSWORD)
             } else {
-                setLoggedInUserSuccess(user_obj)
+                setCurrentUserError(MSG_NO_USER)
             }
         }
     }
@@ -69,11 +65,12 @@ const mapStateToProps = state => ({
 });
 
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    setLoggedInUserError: setLoggedInUserError,
-    setLoggedInUserSuccess: setLoggedInUserSuccess,
-    setUsersPending: setUsersPending,
-    getAllUsers: getAllUsers,
-}, dispatch);
+const mapDispatchToProps = {
+    setCurrentUserPending,
+    setCurrentUserError,
+    setCurrentUserSuccess,
+    setUsersPending,
+    getAllUsers
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(LogInScreen)
