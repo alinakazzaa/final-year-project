78c5b49fd6717ae17ea92bdd527b6e30a23f1ed5
Alina Kazakova <alinakazakova@live.ie>
2020-04-14 00:53:45 +0100

    style & prop types added to loading screen

--- src/components/loading/LoadingScreen.js
+++ src/components/loading/LoadingScreen.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { colors } from '../../styles/base';
 import { ActivityIndicator, View, Text } from 'react-native';
 import { loading } from './styles/loading.styles';
+import PropTypes from 'prop-types'
 
 
-export const LoadingScreen = ({ text }) => {
-    return <View style={loading.bar}>
-        <ActivityIndicator size="large" color={colors.PRIMARY} />
-        <Text style={loading.text}>{text}</Text>
+export const LoadingScreen = ({ text, size }) => {
+    return <View style={size == 'large' ? loading.large : loading.small}>
+        <ActivityIndicator size="large" color={colors.TERTIARY} />
+        {text && <Text style={loading.text}>{text}</Text>}
     </View>
+}
+
+LoadingScreen.propTypes = {
+    text: PropTypes.string,
+    size: PropTypes.string
+}
+
+LoadingScreen.defaultProps = {
+    text: null,
+    size: 'small'
 }
