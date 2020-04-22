47e56500f7822e8f1ee093952ec6a6e69b80352f
Alina Kazakova <alinakazakova@live.ie>
2020-04-22 16:01:19 +0100

    changed date to string

--- src/constants/TodayDate.js
+++ src/constants/TodayDate.js
@@ -1,3 +1,3 @@
-let date = new Date()
+let date = new Date("22/04/2020")
 
-export const DATE_TODAY = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+export const DATE_TODAY = String(date.getDate() + (date.getMonth() + 1) + date.getFullYear())
