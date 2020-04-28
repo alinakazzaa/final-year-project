0178b195a3db98f55fb1cb426a899793610b0067
Alina Kazakova <alinakazakova@live.ie>
2020-04-28 19:56:54 +0200

    layout of list changed

--- src/components/list/InfluencerList.js
+++ src/components/list/InfluencerList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar } from 'react-native-elements'
+import { Avatar, Icon } from 'react-native-elements'
 import { TouchableOpacity, Text, View, ScrollView } from 'react-native'
 import { IconButton } from '../buttons/IconButton'
 import PropTypes from 'prop-types'
@@ -10,79 +10,80 @@ import { colors, base } from '../../styles/base'
 export const InfluencerList = ({ influencers, goToInfluencer, createCollab, removeInfluencer, saveInfluencer }) => {
 
     const influList = (influ, index) => {
-        return (
-            <View style={influencer_style.listItem} key={index}>
-                <View style={influencer_style.header}>
-                    <IconButton
-                        name='chevron-right'
-                        size={50}
-                        color={colors.TERTIARY}
-                        type='material-icons'
-                        onPress={() => goToInfluencer(influ)}
-                    />
-                </View>
-                <View style={influencer_style.middle}>
-                    <View style={influencer_style.middleLeft}>
-                        <Avatar
-                            size={160}
-                            rounded
-                            source={{
-                                uri: influ.profile_pic_url,
-                            }} />
-                        <TouchableOpacity style={influencer_style.linkView} onPress={() => console.log("Go to user profile....")}>
-                            <Text style={influencer_style.linkText}>Instagram</Text>
-                            <IconButton
-                                name='launch'
-                                size={22}
-                                color={colors.TERTIARY}
-                                type='material-icons'
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={influencer_style.middleRight}>
-                        <View style={influencer_style.info}>
-                            <Text style={base.title}>Followers</Text>
-                            <Text style={base.text}>{formatNumber(influ.followers)}</Text>
-                            <Text style={base.title}>No of media</Text>
-                            <Text style={base.text}>{formatNumber(influ.media_count)}</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={influencer_style.footer}>
-                    {influ.to_do && <TouchableOpacity style={influencer_style.voteOption}>
+        if (influ !== null)
+            return (
+                <View style={influencer_style.listItem} key={index}>
+                    <View style={influencer_style.header}>
                         <IconButton
-                            name='check'
+                            name='chevron-right'
                             size={50}
-                            color={colors.GREEN}
-                            type='material-icons'
-                            onPress={() => saveInfluencer(influ)}
-                        /></TouchableOpacity>}
-                    <TouchableOpacity style={influencer_style.createCollab}>
-                        <IconButton
-                            name='create'
-                            size={40}
                             color={colors.TERTIARY}
                             type='material-icons'
-                            onPress={() => createCollab(influ)}
-                        /></TouchableOpacity>
-                    <TouchableOpacity style={influencer_style.voteOption}>
-                        <IconButton
-                            name='close'
-                            size={50}
-                            color={colors.RED}
-                            type='material-icons'
-                            onPress={() => removeInfluencer(influ.id)}
-                        /></TouchableOpacity>
+                            onPress={() => goToInfluencer(influ)}
+                        />
+                    </View>
+                    <View style={influencer_style.middle}>
+                        <View style={influencer_style.middleLeft}>
+                            <Avatar
+                                size={160}
+                                rounded
+                                source={{
+                                    uri: influ.profile_pic_url,
+                                }} />
+                            <TouchableOpacity style={influencer_style.linkView} onPress={() => console.log("Go to user profile....")}>
+                                <Text style={influencer_style.linkText}>Instagram</Text>
+                                <IconButton
+                                    name='launch'
+                                    size={22}
+                                    color={colors.TERTIARY}
+                                    type='material-icons'
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={influencer_style.middleRight}>
+                            <View style={influencer_style.info}>
+                                <Text style={base.title}>Followers</Text>
+                                <Text style={base.text}>{formatNumber(influ.followers)}</Text>
+                                <Text style={base.title}>No of media</Text>
+                                <Text style={base.text}>{formatNumber(influ.media_count)}</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={influencer_style.footer}>
+                        {influ.to_do && <TouchableOpacity style={influencer_style.voteOption}>
+                            <IconButton
+                                name='check'
+                                size={50}
+                                color={colors.GREEN}
+                                type='material-icons'
+                                onPress={() => saveInfluencer(influ)}
+                            /></TouchableOpacity>}
+                        <TouchableOpacity style={influencer_style.createCollab}>
+                            <IconButton
+                                name='account-multiple-plus-outline'
+                                size={60}
+                                color={colors.TERTIARY}
+                                type='material-community'
+                                onPress={() => createCollab(influ)}
+                            /></TouchableOpacity>
+                        <TouchableOpacity style={influencer_style.voteOption}>
+                            <IconButton
+                                name='close'
+                                size={50}
+                                color={colors.RED}
+                                type='material-icons'
+                                onPress={() => removeInfluencer(influ.id)}
+                            /></TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-        )
+            )
     }
 
     return (
 
-        <View>
+        <View style={{ marginBottom: 300 }}>
             <ScrollView keyboardDismissMode='on-drag'
-                contentContainerStyle={influencer_style.scrollContainer}>
+                contentContainerStyle={base.scrollContainer}>
                 {influencers.map((i, index) => {
                     return influList(i, index)
                 })}
