ff6e019e36bdeca6a303748f59d16130c5f7587d
Alina Kazakova <alinakazakova@live.ie>
2020-04-29 00:59:58 +0200

    slider initial values updated

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,11 +4,12 @@ import { colors } from '../../styles/base'
 import RangeSlider from 'rn-range-slider'
 import { View } from 'native-base'
 import { sliderStyle } from './styles/slider.styles'
+import { followerRanges } from '../../constants/criteria'
 
 export default class Slider extends React.Component {
 
     render() {
-        const { min, max, step, onChange } = this.props
+        const { min, max, step, onChange, initialMin, initialMax } = this.props
         return (<RangeSlider
             style={sliderStyle.bar}
             lineWidth={23}
@@ -19,8 +20,8 @@ export default class Slider extends React.Component {
             thumbBorderWidth={2}
             min={min}
             max={max}
-            initialLowValue={min}
-            initialHighValue={max}
+            initialLowValue={initialMin}
+            initialHighValue={initialMax}
             step={step}
             onValueChanged={(low, high) => {
                 onChange(low, high)
@@ -34,17 +35,17 @@ export default class Slider extends React.Component {
 Slider.propTypes = {
     min: PropTypes.number,
     max: PropTypes.number,
-    initial_max: PropTypes.number,
-    initial_min: PropTypes.number,
+    initialMax: PropTypes.number,
+    initialMin: PropTypes.number,
     step: PropTypes.number,
     onChange: PropTypes.func,
 }
 
 Slider.defaultProps = {
-    min: 0,
-    max: 1000000,
-    initial_max: 0,
-    initial_min: 1000000,
+    min: followerRanges.micro.min,
+    max: followerRanges.micro.max,
+    initialMax: followerRanges.micro.min,
+    initialMin: followerRanges.micro.max,
     step: 1000,
     onChange: null,
 }
