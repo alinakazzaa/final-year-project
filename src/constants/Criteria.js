135ae3140efed95884fab2371c7523d4bccf2f42
Alina Kazakova <alinakazakova@live.ie>
2020-04-29 13:46:58 +0200

    changed how criteria looks (exact number to fetch)

--- src/constants/Criteria.js
+++ src/constants/Criteria.js
@@ -1,12 +1,12 @@
 export const followerRanges = {
     micro: { min: 5000, max: 100000 },
-    midi: { min: 100000, max: 1000000 },
-    macro: { min: 1000000, max: 5000000 }
+    midi: { min: 100000, max: 500000 },
+    macro: { min: 500000, max: 1000000 }
 }
 
 export const numberOfProfiles = {
-    10: '10 +',
-    20: '20 +',
-    30: '30 +',
-    50: '50 +'
+    10: '10',
+    20: '20',
+    30: '30',
+    50: '50'
 }
