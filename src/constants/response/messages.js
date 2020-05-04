3b78f84895c7a5215b1e1d9656c442ec72b161df
Alina Kazakova <alinakazakova@live.ie>
2020-05-04 22:07:27 +0200

    new response types

--- src/constants/response/messages.js
+++ src/constants/response/messages.js
@@ -4,7 +4,8 @@
 export const MSG_NO_USERS = 'No users yet registered.'
 export const MSG_NO_USER = 'User not found. Please register.'
 export const MSG_INCORRECT_PASSWORD = 'Incorrect password. Please try again'
-export const MSG_EMPTY_FIELDS = 'Username and Password cannot be empty.'
+export const MSG_EMPTY_FIELDS = 'All fields must be filled.'
+export const MSG_PASSWORDS_DONT_MATCH = 'Passwords don\'t match.'
 
 
 // PROJECT
