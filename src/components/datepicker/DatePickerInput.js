07a6f82325bd9c09cf0d65d1e98d6a8e19cea01f
Alina Kazakova <alinakazakova@live.ie>
2020-05-05 21:45:25 +0200

    removed unused code

--- src/components/datepicker/DatePickerInput.js
+++ src/components/datepicker/DatePickerInput.js
@@ -2,7 +2,6 @@ import React from 'react'
 import DatePicker from 'react-native-datepicker'
 import { DATE_TODAY } from '../../constants/TodayDate'
 import { inputView } from '../../styles/form'
-import { fonts, colors, dimensions } from '../../styles/base'
 
 export const DatePickerInput = ({ date, handleChange }) => {
 
@@ -18,28 +17,6 @@ export const DatePickerInput = ({ date, handleChange }) => {
             confirmBtnText="Save"
             cancelBtnText="Cancel"
             showIcon={false}
-            customStyles={{
-                placeholderText: {
-                    color: colors.TERTIARY,
-                    fontWeight: fonts.WEIGHT_LEIGHT,
-                    fontSize: fonts.MEDIUM,
-                    borderBottomWidth: 0
-                },
-                dateText: {
-                    color: colors.TERTIARY,
-                    fontWeight: fonts.WEIGHT_LEIGHT,
-                    fontSize: fonts.MEDIUM
-
-                },
-                dateInput: {
-                    ...inputView,
-                    borderRadius: 4,
-                    width: dimensions.fullWidth * 0.4,
-                    borderColor: colors.BORDER,
-                    borderWidth: 0.7
-                }
-                // ... You can check the source to find the other keys.
-            }}
             onDateChange={date => handleChange(date)}
         />
     )
